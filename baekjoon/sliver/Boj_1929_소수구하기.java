239eb7e8d057828304940a1e70f7d29084a11c9b
eosun77 <eosun77@gmail.com>
2022-09-15 22:03:38 +0900

    add

---
+++ baekjoon/sliver/Boj_1929_소수구하기.java
@@ -0,0 +1,26 @@
+package baekjoon.sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1929_소수구하기 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		for(int i=N;i<=M;i++) {
+			if(isPrime(i)&&i!=1) System.out.println(i);
+		}
+	}
+	
+    public static boolean isPrime(int num){
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
