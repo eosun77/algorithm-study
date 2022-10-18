357086d61b6207f411750fcbe533cbcd6a2f2817
eosun77 <eosun77@gmail.com>
2022-10-19 01:08:20 +0900

    BOJ_11052_카드구매하기_DS

---
+++ baekjoon/sliver/Boj_11048_이동하기.java
@@ -0,0 +1,12 @@
+package baekjoon.sliver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj_11048_이동하기 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
